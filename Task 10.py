29d2f2068f9c89186e853f81a29cd73a61a5a872
kost960 <pozitq3@gmail.com>
2024-04-19 17:16:43 +0700

    Fix 10

--- Task 10.py
+++ Task 10.py
@@ -12,14 +12,3 @@ def kl(k, n):
 
 n = int(input('Введите количество кубиков N: '))
 print(kl(0, n))
-
-def combinations(k, n):
-    if k == 0:
-        return 1
-    if k < 0 or n == 0:
-        return 0
-    return combinations(k - n, n - 1) + combinations(k, n - 1)
-
-
-k = int(input('Введите количество кубиков K: '))
-print(combinations(k, k))
